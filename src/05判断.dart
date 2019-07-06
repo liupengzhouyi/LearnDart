44ccca0d3303b47cc592bc842250b8020caf8ae3
liupeng <LIUPENG.0@outlook.com>
2019-07-06 16:47:44 +0800

    Learn Dart V 1.0.4 判断

---
+++ src/05判断.dart
@@ -0,0 +1,19 @@
+void main() {
+	if (2==3) {
+	    print('2==3');
+	} else {
+		print('2!=3');
+	}
+
+	int number = 2;
+	switch(number) {
+		case 1: {
+			print(1);
+			break;
+		}
+		case 2: {
+			print(2);
+			break;
+		}
+	}
+}
