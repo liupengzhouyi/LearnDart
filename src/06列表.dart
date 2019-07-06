9129ace7d12b372bfe3ca766e1eda05f1186af73
liupeng <LIUPENG.0@outlook.com>
2019-07-06 16:55:01 +0800

    Learn Dart V 1.0.5 列表

---
+++ src/06列表.dart
@@ -0,0 +1,15 @@
+void main() {
+	var list = new List(3);
+	list[0] = 123;
+	list[1] = 234;
+	list[2] = 345;
+	print(list);
+
+	var newList = new List();
+	newList.add(121);
+	newList.add(122);
+	newList.add(123);
+	newList.add(124);
+	print(newList);
+
+}
