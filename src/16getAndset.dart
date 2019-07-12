878cdcaaa3e1c4f2a247b042334213836c1e073b
liupeng <LIUPENG.0@outlook.com>
2019-07-12 11:47:06 +0800

    Learn Dart V 1.1.4 get&set

---
+++ src/16getAndset.dart
@@ -0,0 +1,19 @@
+class Class16 {
+	// 表示私有变量
+	String _name;
+	// 表示私有变量
+	int _age;
+
+	String get name => _name;
+
+	set name(String value) {
+		_name = value;
+	}
+
+	int get age => _age;
+
+	set age(int value) {
+		_age = value;
+	}
+
+}
