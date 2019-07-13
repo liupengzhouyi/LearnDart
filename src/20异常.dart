dff37e566c9ecd9ae85dd785f748eeeb13ecb99c
liupeng <LIUPENG.0@outlook.com>
2019-07-13 18:28:56 +0800

    Learn Dart V 1.1.9 异常

---
+++ src/20异常.dart
@@ -0,0 +1,56 @@
+void main() {
+	int x = 10;
+	int y = 0;
+	int number;
+
+	// try ... on
+	try {
+		number = x ~/ y;
+	} on IntegerDivisionByZeroException {
+		print('除数不能为0');
+	}
+
+	// try ... catch
+	try {
+		number = x ~/ y;
+	} catch(e) {
+		print(e);
+	}
+
+	// try ... on ... catch
+	try {
+		number = x ~/ y;
+	} on IntegerDivisionByZeroException catch(e) {
+		print(e);
+	}
+
+	// finally
+	// 无条件执行可选的finally块
+	try {
+		number = x ~/ y;
+	} on IntegerDivisionByZeroException {
+		print('除数不能为0');
+	} catch (e) {
+	  print(e);
+	} finally {
+		print('程序结束');
+	}
+
+	// 抛出异常
+	try {
+		test(-12);
+	} catch (e) {
+	    print(e);
+	}
+
+}
+
+void test(int age) {
+	if (age < 0) {
+		throw new FormatException();
+	}
+}
+
+
+
+
